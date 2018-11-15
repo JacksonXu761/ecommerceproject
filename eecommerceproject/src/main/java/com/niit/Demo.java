e9589eef245e324c99c85f20ed310217d115832b
Jackson <983306932@qq.com>
2018-11-15 11:37:23 +0800

    jackson second commit

--- eecommerceproject/src/main/java/com/niit/Demo.java
+++ eecommerceproject/src/main/java/com/niit/Demo.java
@@ -2,7 +2,10 @@ package com.niit;
 
 public class Demo {
 
-    public static void main(String args[]) {
+    public static void main(String args[])
+    {
+        System.out.print("xgfsew");
         System.out.print("commit from ethan...");
     }
+
 }
