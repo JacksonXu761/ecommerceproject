9a2678a84c9181eefcd57c441aaa54012b4358c2
Jackson <983306932@qq.com>
2018-11-15 11:46:39 +0800

    jackson second commit

--- eecommerceproject/src/main/java/com/niit/Demo.java
+++ eecommerceproject/src/main/java/com/niit/Demo.java
@@ -4,7 +4,7 @@ public class Demo {
 
     public static void main(String args[])
     {
-        System.out.print("xgfsew");
+        System.out.print("jackson second commit");
         System.out.print("commit from ethan...");
     }
 
